e08628b6d6cc9f6cdb8e70f0587549f752fa2f9f
leesy24 <leesy24@ubuntu-16.04-64bit>
2021-04-21 19:48:58 +0900

    [DWS4000I] assign version to v4.35

--- target/linux/x86/generic/profiles/002-DWS4000.mk
+++ target/linux/x86/generic/profiles/002-DWS4000.mk
@@ -7,7 +7,7 @@
 
 define Profile/DWS4000I
 	NAME:=DWS4000 for Intel
-	VERSION:=v4.34
+	VERSION:=v4.35
 	PACKAGES:= \
 		avahi-daemon-service-http avahi-daemon-service-ssh avahi-nodbus-daemon \
 		bc \
